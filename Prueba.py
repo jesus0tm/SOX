1c3cfdb5cb1a767f4fb1da1ef52f7c4a9a2917f3
jessu <jtroya@alumnes.aulabalear.org>
2023-10-29 11:40:18 +0100

    Prueba y error

---
+++ Prueba.py
@@ -0,0 +1,14 @@
+    #def metodo1():
+
+    #   print ("Hola Mundo")
+
+    #def metodo2():
+
+    #   print ("¿Como va todo?") 
+
+    def a():
+        4
+    def b():
+        5
+    def suma(a,b):
+        print(a-b)
