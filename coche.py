e19c9edbe9fefade6dddf415f351ac45cecb5323
jessu <jtroya@alumnes.aulabalear.org>
2023-11-17 19:39:56 +0100

    ola

--- coche.py
+++ coche.py
@@ -4,11 +4,11 @@ class coche:
         self.ruedas = ruedas
         self.puertas = puertas
         self.velocidad = velocidad
-    
+    def arrancar(print, arrancar, self):
+        print("Voy a arrancar")
 
-c1 = coche('V20', '4', '4', '120 km/h')
+c1.arrancar
+print (c1.arrancar)
 
-print (c1.motor)
-print (c1.ruedas)
-print (c1.puertas)
-print (c1.velocidad)
+
+
---
+++ coche2.py
@@ -0,0 +1,20 @@
+class car:
+    def __init__(self, motor, ruedas, puertas, velocidad):
+        self.motor = motor
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.vevlocidad = velcidad
+        
+    def arrancar(self):
+        print("Va ha salir cagando gasolina")
+        
+
+
+c1 = car("V6", 4, 4, 80)
+c1.arrancar()
+
+
+print(c1.motor)
+print(c1.ruedas)
+print(c1.puertas)
+print(c1.velocidad)
