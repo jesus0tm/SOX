19c43fb302e498c2c662d0c8406b05973cc4d8e6
jessu <jessu@gmail.com>
2023-11-27 00:34:53 +0100

    ola

---
+++ ejercicios.py
@@ -0,0 +1,82 @@
+#Parte de ejrcicios
+
+
+class nomb:
+    def __init__(self,  nombre1, nombre2, nombre3, nombre4)
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        self.nombre3 = nombre3
+        self.nombre4 = nombre4
+
+from ast import While
+
+def busqueda_while (arr) :
+
+i = [1, 2, 3]
+
+def __init__(self, not_encontrado):
+    self.array = []
+    self.not_encontrado = []
+    self.arr
+    
+def busqueda_while (arr) :
+
+def var_encontrar(self, var_encontrar):
+    self.var_encontrar = []
+    
+def busqueda_while(self):
+    self.busqueda_while
+
+n.1 = nomb("Macia", "Juan", "Jose", "Ernes")
+
+n1.salgan
+
+def arr = ["Macia", "Juan", "Jose", "Ernes", busqueda_while, recorrido_while, recorrido _for]
+
+if __name__ == "__main__":
+    arr = ["Macia", "Juan", "Jose", "Ernes"]
+    for elemento in arr:
+        if "Juan" : elemento
+        print("Encontrado")
+    while for elemento in arr:
+        if "Valor_encontrado":elemento
+        print("Encontrado")
+    while elemento in arr:
+        for elemento in arr:
+        if "Juan" : elemento
+        print("Encontrado")
+    
+
+
+
+'''''
+# arr = ["Macia", "Juan", "Jose", "Ernes"]  
+
+# 1. var_a_encontrado = "juan"
+
+    for var_encontrar in arr:
+        if "Juan" : arr
+        print("Encontrado")
+
+# 2. def busqueda_while (arr) :
+
+
+
+# 3. def busqueda_for (arr, var_encontrar)
+
+    for elemento in arr:
+        if "Juan" : elemento
+        print("Encontrado")
+
+# 4. def recorrido_while (arr)
+    while elemento i arr:
+        for elemento in arr:
+        print (elemento)
+
+# 5. def recorrido_for (arr)
+
+    while for elemento in arr:
+        if "Valor_encontrado":elemento
+        print("Encontrado")
+
+'''
